3eb1ee14b6ae10938705e62e94d7a53e625bb05f
BuildTools <davidmaamoaix@gmail.com>
2023-03-23 23:12:30 +0800

    removed unsound approximation plans

--- singleline/analysis/dataflow.py
+++
@@ -1,9 +0,0 @@
-class FunctionDataFlow:
-    """
-    A global graph that traces the result of dataflow analysis of functions.
-    Only functions are of interest here due to how their invocation causes
-    their free variables to be live in the call site.
-
-    Essentially, each function `f` must track down all variables (set `V`)
-    in all scopes that might be assigned `f`. Then every function is 
-    """
