a44abf4b71cdcdc9188e9ae166f9f3fc41de94fd
BuildTools <davidmaamoaix@gmail.com>
2023-03-23 23:11:59 +0800

    static analysis

--- singleline/analysis/liveness.py
+++ singleline/analysis/liveness.py
@@ -3,7 +3,10 @@ from typing import Dict, Set
 
 """
 Note to self: a function call on `f` at line `n` makes the set of
-free variables in `f` live on line `n - 1`.
+free variables in `f` live on line `n - 1`. Due to scoping this makes
+the liveness of a variable propagate up the call stack, (e.g., if `x`
+is live in `foo()`, and `bar()` might call `foo()`, then `x` is also
+live in `bar()`).
 
 SCOPE MATTERS!!! Encode the function scope along side with the variable
 names in liveness & reference resolver.
@@ -22,9 +25,11 @@ class LivenessData:
     
     # tracked_vars[var_name] = { <live at lines> }
     tracked_vars: Dict[str, Set[int]]
+    live_out: Set[str]
 
     def __init__(self):
         self.tracked_vars = {}
+        self.live_out = set()
 
     def live_at(self, var: str, line: int):
         return line in self.tracked_vars[var]
