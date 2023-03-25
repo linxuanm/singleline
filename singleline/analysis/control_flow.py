449475b36e5dbc402d2c9b3e4ace09d36080d549
BuildTools <davidmaamoaix@gmail.com>
2023-03-25 20:58:27 +0800

    InterruptVisitor class

--- singleline/analysis/control_flow.py
+++ singleline/analysis/control_flow.py
@@ -5,13 +5,38 @@ from typing import List
 from ..misc.types import VRet
 
 
+# A hashable wrapper for `List[ast.AST]`.
+class NodeBundle:
+
+    bundle: List[ast.AST]
+
+    def __init__(self, bundle: List[ast.AST] = None):
+        if bundle is None: bundle = []
+
+        self.bundle = bundle
+
+    def append(self, node: ast.AST):
+        self.bundle.append(node)
+
+
 class ControlFlowGraph:
     """
     Generates the control flow graph of the source program so that lambda
     and branching structures can be determined statically.
     """
 
-    graph: nx.DiGraph
+    graph: nx.classes.DiGraph
 
     def __init__(self):
-        self.graph = nx.DiGraph()
+        self.graph = nx.classes.DiGraph()
+
+        # entry point
+        self.graph.add_node('main')
+
+    def analysis_pass(self, code: List[ast.AST]):
+        batch = NodeBundle()
+        for node in code:
+            pass
+
+    def _is_compound_node(self, node: ast.AST):
+        types = [ast.If, ast.For, ast.While]
---
+++ singleline/analysis/interrupt.py
@@ -0,0 +1,54 @@
+import ast
+
+from ..misc.types import VRet
+
+
+class InterruptVisitor(ast.NodeVisitor):
+    """
+    Responsible for determining various interrupt statements in an
+    `ast.AST` and its children. It determines if an interrupt can be
+    possibly reached in a control flow, and is used to generate the
+    necessary flags for a loop.
+    """
+
+    has_return: bool
+    has_break: bool
+    func_stack: int
+    loop_stack: int
+
+    # Note that `has_continue` does not exist as it does not require an
+    # external flag to be defined, and can be encoded in the `while` lambda
+    # instead (by returning `False` to `f` in `next(filter(f, ...)))`.
+
+    def __init__(self):
+        self.has_return = False
+        self.has_break = False
+        self.func_stack = 0
+        self.loop_stack = 0
+    
+    def visit_Return(self, node: ast.Return) -> VRet:
+        if self.func_stack == 0:
+            self.has_return = True
+
+        self.generic_visit(node)
+
+    def visit_Break(self, node: ast.Break) -> VRet:
+        if self.loop_stack == 0:
+            self.has_break = True
+
+        self.generic_visit(node)
+    
+    def visit_FunctionDef(self, node: ast.FunctionDef) -> VRet:
+        self.func_stack += 1
+        self.generic_visit(node)
+        self.func_stack -= 1
+
+    def visit_For(self, node: ast.For) -> VRet:
+        self.loop_stack += 1
+        self.generic_visit(node)
+        self.loop_stack -= 1
+
+    def visit_While(self, node: ast.While) -> VRet:
+        self.loop_stack += 1
+        self.generic_visit(node)
+        self.loop_stack -= 1
