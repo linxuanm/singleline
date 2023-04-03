6e357c4ab50da717d380953d1f6c85a2f4ff1ff8
BuildTools <davidmaamoaix@gmail.com>
2023-04-03 12:12:22 +0800

    Successors path search

--- singleline/transform/graph_utils.py
+++ singleline/transform/graph_utils.py
@@ -1,6 +1,8 @@
 import ast
 import networkx as nx
-from typing import Union
+from typing import Set, Dict, Tuple
+
+from ..analysis import CFGLabels
 
 
 def get_last_convergence(graph: nx.classes.DiGraph, node: ast.AST) -> ast.AST:
@@ -10,5 +12,26 @@ def get_last_convergence(graph: nx.classes.DiGraph, node: ast.AST) -> ast.AST:
     the last such node.
 
     If no sucessors converge, returns the given node.
+
+    TODO: find a better algorithm for this O(N^2) abomination.
     """
+
+    def _get_successors(node: ast.AST):
+        return (
+            i for i in graph.successors(node)
+            if graph[node][i]['label'] != CFGLabels.IGNORE
+        )
+    
+    def _search_path(node: ast.AST) -> Dict[ast.AST, int]:
+        front = [node]
+        visited = {node: 0}
+        while front:
+            curr = front.pop()
+            for succ in _get_successors(curr):
+                if curr not in visited:
+                    visited[succ] = visited[curr] + 1
+                    front.append(succ)
+        
+        return visited
+    
     pass
