69ee0168ea79ba2a4b5d861bbf692077fc32d568
BuildTools <davidmaamoaix@gmail.com>
2023-04-07 12:03:09 +0800

    COMPLEX function test case for CFG analysis

--- test/test_cfg.py
+++ test/test_cfg.py
@@ -20,7 +20,7 @@ COMPLEX_FUNC = """
 def foo():
     a = a + 1
     if a == 2:
-        return 'television'
+        c = 2
     elif a == 3:
         for i in range(10):
             if i > 5:
@@ -52,11 +52,11 @@ class ControlFlowGraphTest(unittest.TestCase):
         tree, id_gen = singleline.analysis.preprocess(COMPLEX_FUNC)
         singleline.analysis.control_flow_pass(tree)
 
-        print(id_gen.invalid_pool)
+        graph: nx.classes.DiGraph = tree.body[0].graph
 
-        graph = tree.graph
-
-        last = singleline.misc.get_last_convergence(graph, tree)
+        last = singleline.misc.get_last_convergence(graph, tree.body[0])
+        for i, ans in zip(last.bundle, ['b=3', 'print(b)']):
+            self.assertEqual(ast.unparse(i).replace(' ', ''), ans)
 
 
 if __name__ == '__main__':
