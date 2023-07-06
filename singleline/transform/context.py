640db95a892fa7515d65b2e8aa7b13f983c8f144
BuildTools <davidmaamoaix@gmail.com>
2023-07-06 22:17:28 +0800

    checkpoint

--- singleline/transform/context.py
+++ singleline/transform/context.py
@@ -18,7 +18,11 @@ class LocalVariableVisitor(ast.NodeVisitor):
     1. Find all mutated variables in the loop. For the ones that are contained
     in `LocalVariableVisitor` before the loop, initialize the store list to
     their value. Otherwise, set their initial value to `None`.
-    2. 
+    2. Replace all occurrences of mutated variables in the loop with the store
+    list (for both storing and using). This replacement should propagate into
+    nested function definitions until one layer of function definition mutates
+    the value of the variable.
+    3. Recursively transpiles the 
     """
 
     vars: Set[str]
